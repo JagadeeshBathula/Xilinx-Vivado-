d7392a9161183ff6969b63c4ebb73c2a148efbcb
Jagadeesh Bathula <jagadeeshbattula0@gmail.com>
2025-07-09 18:39:37 +0530

    Add files via upload

---
+++ 8 x 1 MUX/mux8_1.srcs/sim_1/new/simulation.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+
+module simulation;
+
+  reg [7:0] i;
+  reg [2:0] s;
+  wire y;
+
+  // Instantiate the MUX (make sure the module name 'mux8_1' is correct)
+  mux8_1 dut (
+    .i(i),
+    .s(s),
+    .y(y)
+  );
+
+  initial begin
+    $display("t=%0t | i = %b | s = %b | y = %b", $time, i, s, y);
+    repeat(10) begin
+      i = $random;
+      s = $random;
+      #10;  // Add time delay to see output
+      $display("t=%0t | i = %b | s = %b | y = %b", $time, i, s, y);
+    end
+    $finish;
+  end
+
+endmodule
