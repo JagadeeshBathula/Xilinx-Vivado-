37eb3f1b60e836cf9c364f84686e8c2748a75788
Jagadeesh Bathula <jagadeeshbattula0@gmail.com>
2025-07-27 21:07:33 +0530

    Add files via upload

---
+++ 4-1 by 2-1 MUX/4-1 by 2-1 mux.srcs/sources_1/new/mux4_1by2_1.v
@@ -0,0 +1,15 @@
+`timescale 1ns / 1ps
+
+
+module mux4_1by2_1(
+    input a,b,c,d,s1,s2,
+    output y
+    );
+    
+    wire y1 , y2;
+    
+    mux2_1 mux2_1_1(a,b,s1,y1);
+    mux2_1 mux2_1_2(c,d,s1,y2);
+    mux2_1 mux2_1_3(y1,y2,s2,y);
+    
+endmodule
